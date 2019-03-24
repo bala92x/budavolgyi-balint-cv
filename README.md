9b8d4cbcf294c7a1930fc48913c6cbb6ee5ea885
bala92x <bala92x@gmail.com>
2019-03-24 19:16:21 +0100

    Add testing frameworks to stack and add portfolio

--- README.md
+++ README.md
@@ -22,6 +22,12 @@ I am a junior web developer with experience in planning, building, and maintaini
     
 Welcome to my CV!
 
+- [Education](#education)
+- [Experience](#experience)
+- [Tech Stack](#tech-stack)
+- [Portfolio](#portfolio)
+- [Contact](#contact)
+
 ### Education
 
 - Web development and online marketing certifications - [https://github.com/bala92x/certs](https://github.com/bala92x/certs)
@@ -44,7 +50,8 @@ Welcome to my CV!
   - [https://kameleonline.hu](https://kameleonline.hu)
 - Own side projects - since 2017 January
 
-### Tech Stack (years of experience in the installable version)
+### Tech Stack
+(years of experience in the installable version)
 
 - Javascript
   - Vanilla JS
@@ -66,6 +73,44 @@ Welcome to my CV!
 - CSS3
   - Bootstrap
   - SCSS
+
+### Portfolio
+
+- Open Source
+    - chat-app
+        - description: Simple Node.js based chat app with socket.io integration
+        - source: https://github.com/bala92x/chat-app
+        - url: https://brit-tudosok-chat.herokuapp.com
+    - score-keeper
+        - description: Simple score keeper app with Meteor and React
+        - source: https://github.com/bala92x/score-keeper
+        - url: https://bala-score-keeper.herokuapp.com
+    - cli-weather-app
+        - description: Simple Node.js cli weather app, that gets temperature information about the given location
+        - source: https://github.com/bala92x/cli-weather-app
+    - task-manager
+        - description: Node.js task manager API with JWT authorization
+        - source: https://github.com/bala92x/task-manager
+        - url: https://bala-task-manager.herokuapp.com
+    - More open source projects: https://github.com/bala92x
+- Projects I contributed to
+    - Agora Lakópark
+        - description: Website of an 11 building residential park, with complex administration backend
+        - url: https://agorapark.hu
+        - framework: Laravel 5.3
+    - Pannon Oktatási Központ
+        - description: Website of an educational institution, with administration backend and the opportunity of signing up for courses
+        - url: https://www.oktatasioldal.hu
+        - framework: Laravel 5.4
+    - Auto Finesse webshop
+        - description: Webshop of car detailing products. Hundreds of products, several payment options and Facebook Shop synchronization
+        - url: https://afshop.hu
+        - CMS: WordPress
+    - Micsoda esküvő
+        - description: Portfolio site of a marriage celebrant
+        - url: https://kulonlegesszertartas.hu
+        - CMS: WordPress
+    - More projects I contributed to: https://brandingpage.hu
   
 ### Contact
 
--- src/resume.js
+++ src/resume.js
@@ -93,6 +93,14 @@ const resume = {
                 {
                     "name": "Handlebars",
                     "experience": getDiff('2018-12-05')
+                },
+                {
+                    "name": "Mocha",
+                    "experience": getDiff('2018-12-10')
+                },
+                {
+                    "name": "Jest",
+                    "experience": getDiff('2019-01-05')
                 }
             ]
         },
@@ -138,6 +146,71 @@ const resume = {
                 {
                     "name": "SCSS",
                     "experience": getDiff('2018-10-10')
+                }
+            ]
+        }
+    ],
+    "Portfolio": [
+        {
+            "name": "Open Source",
+            "sub-portfolios": [
+                {
+                    "name": "chat-app",
+                    "description": "Simple Node.js based chat app with socket.io integration",
+                    "source": "https://github.com/bala92x/chat-app",
+                    "url": "https://brit-tudosok-chat.herokuapp.com" 
+                },
+                {
+                    "name": "score-keeper",
+                    "description": "Simple score keeper app with Meteor and React",
+                    "source": "https://github.com/bala92x/score-keeper",
+                    "url": "https://bala-score-keeper.herokuapp.com"
+                },
+                {
+                    "name": "cli-weather-app",
+                    "description": "Simple Node.js cli weather app, that gets temperature information about the given location",
+                    "source": "https://github.com/bala92x/cli-weather-app",
+                },
+                {
+                    "name": "task-manager",
+                    "description": "Node.js task manager API with JWT authorization",
+                    "source": "https://github.com/bala92x/task-manager",
+                    "url": "https://bala-task-manager.herokuapp.com"
+                },
+                {
+                    "more": "More open source projects: https://github.com/bala92x"
+                }
+            ],
+        },
+        {
+            "name": "Projects I contributed to",
+            "sub-portfolios": [
+                {
+                    "name": "Agora Lakópark",
+                    "description": "Website of an 11 building residential park, with complex administration backend",
+                    "url": "https://agorapark.hu",
+                    "framework": "Laravel 5.3"
+                },
+                {
+                    "name": "Pannon Oktatási Központ",
+                    "description": "Website of an educational institution, with administration backend and the opportunity of signing up for courses",
+                    "url": "https://www.oktatasioldal.hu",
+                    "framework": "Laravel 5.4"
+                },
+                {
+                    "name": "Auto Finesse webshop",
+                    "description": "Webshop of car detailing products. Hundreds of products, several payment options and Facebook Shop synchronization",
+                    "url": "https://afshop.hu",
+                    "CMS": "WordPress"
+                },
+                {
+                    "name": "Micsoda esküvő",
+                    "description": "Portfolio site of a marriage celebrant",
+                    "url": "https://kulonlegesszertartas.hu",
+                    "CMS": "WordPress"
+                },
+                {
+                    "more": "More projects I contributed to: https://brandingpage.hu"
                 }
             ]
         }
